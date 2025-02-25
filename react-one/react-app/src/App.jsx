d03768b6f44eb6c6713d2bbe66d4082fb5292c19
rahoolkar <batman30jan2002@gmail.com>
2025-02-25 17:19:03 +0530

    react 1 completed

---
+++ react-one/react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { ProductList } from "./ProductList.jsx";
+
+//creating our first component
+
+
+function App(){
+  return (
+    <ProductList></ProductList>
+  )
+}
+
+export default App
---
+++ react-one/react-app/src/ProductList.jsx
@@ -0,0 +1,13 @@
+import { Product } from "./Title.jsx"
+
+function ProductList () {
+    return (
+        <>
+            <Product></Product>
+            <Product></Product>
+            <Product></Product>
+        </>
+    )
+}
+
+export {ProductList} ;
