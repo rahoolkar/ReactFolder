882f60818c29fd4f692604d7fbbb896c509ee409
rahoolkar <batman30jan2002@gmail.com>
2025-02-27 14:43:01 +0530

    handling non-click events

--- react-three/src/App.jsx
+++ react-three/src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css'
 import Button from "./button.jsx";
 
+function hover(){
+  console.log("mouse entered into paragraph")
+}
+
 function App() {
-  return <Button></Button>;
+  return (
+    <>
+      <Button></Button>
+      <p onMouseOver={hover}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam officiis atque corrupti aliquid repellendus eius eveniet modi odio repellat nulla?</p>
+    </>
+  );
 }
 
 export default App
