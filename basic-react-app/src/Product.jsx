180b9ebeebc05a5a0511ae03614c79b47f3ce1d1
rahoolkar <batman30jan2002@gmail.com>
2025-03-26 19:58:28 +0530

    react part2 done

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,10 +1,24 @@
 import "./Product.css";
 
-function Product() {
+function Product({ title, price, options = [] }) {
+  let isDiscount = price > 30000;
+  options = options.map((option) => {
+    return <li>{option}</li>;
+  });
+  let styles = {
+    backgroundColor: isDiscount ? "red" : "yellow",
+  };
   return (
-    <div className="Product">
-      <h1>Product Title</h1>
+    <div className="Product" style={styles}>
+      <h1>{title.toUpperCase()}</h1>
       <h5>This is Product Description</h5>
+      <p>Item Price : {price.toLocaleString()}</p>
+      {isDiscount ? (
+        <p>Discount of 5% available</p>
+      ) : (
+        <a href="https://www.google.com">Get Discount</a>
+      )}
+      <p>{options}</p>
     </div>
   );
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,14 +1,16 @@
 import Product from "./Product";
 
 function ProductTab() {
+    let options = ["hi-tech","electronics","durable"];
   return (
     <>
       <h3>This is a Product Tab</h3>
-      <Product></Product>
-      <Product></Product>
-      <Product></Product>
+      <Product title="Laptop" price={4000} options={options}></Product>
+      <Product title="Mobile" price={10000}></Product>
+      <Product title="Headphones" price={5000}></Product>
     </>
   );
 }
 
 export default ProductTab;
+// options2={{a:"hello",b:"meow"}}
